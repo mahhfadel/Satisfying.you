14ac9ff9faeba9b6118c28fd20aa67a832735140
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2023-09-01 11:30:06 -0300

    cronometro coleta com bug

--- scripts/scriptAgradecimento.js
+++ scripts/scriptAgradecimento.js
@@ -2,11 +2,27 @@
 function iniciaModal(modalID) {
 	const modal = document.getElementById(modalID)
     modal.classList.add('mostrar');
+    timer()
     setTimeout(function(){
         modal.classList.remove('mostrar');
+        window.location.href = "../pages/pageColeta.html"
     }, 3000)
 }
 
+function timer() {
+    var timer = document.getElementById('timer')
+    contador = 3
+
+    timer.innerHTML = contador
+
+    if(contador > 0) {
+        setInterval(function(){
+            contador--
+            timer.innerHTML = contador
+            console.log(contador)
+        }, 1000)
+    }
+}
 
 const pessimo = document.getElementById('pessimo')
 pessimo.addEventListener('click', () => {
